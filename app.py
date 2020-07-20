183d672df173303d14b67bf0d04e8e8082fb63db
Prachi Swar <pswar6519@bths.edu>
2020-07-20 14:37:44 -0400

    add more

--- app.py
+++ app.py
@@ -4,25 +4,12 @@ from flask import Flask
 from flask import render_template
 from flask import request
 from flask import session
-
-
-import requests
-import random
-API_endpoint = "https://jservice.io/api/clues"
-API_query = "value=1000"
-API_url = API_endpoint +"?"+ API_query
-r = requests.get(API_url)
-data = r.json()
-print(API_url)
-
-import requests #To access our API
-
+import requests 
+import model
 # -- Initialization section --
 app = Flask(__name__)
-
 ## secret key for session (In production, you would set this key via an environment variable)
 app.secret_key = b'HO\xf8\xff+\n\x1e\\~/;}'
-
 # -- Routes section --
 @app.route('/')
 @app.route('/index')
@@ -31,11 +18,29 @@ def index():
     return render_template('index.html')
 @app.route("/random")
 def jeopardy_random():
-    ## Use jservice API/random to get 1 jeopardy clue
-    return render_template("random_clue.html")
+    API_endpoint2="https://jservice.io/api/random"
+    r=requests.get(API_endpoint2)
+    data = r.json()
+    return render_template("random_clue.html",data=data)
+@app.route('/answerJeopardy', methods=['GET', 'POST'])
+def answerJeopardy():
+    if request.method=='GET':
+        return ""
+    else:
+        model.display_clue(r)
+#To access our API
+# API_endpoint = "https://jservice.io/api/clues"
+# API_query = "value=1000"
+# API_url = API_endpoint +"?"+ API_query
+# r = requests.get(API_url)
+# data = r.json()
+# print(API_url)
 
 
-   
+
+
+
+
 
 
 
--- model.py
+++ model.py
@@ -1,3 +1,7 @@
+
+import requests
+import random
+
 def display_clue(clue):
     clue_category = clue["category"]["title"]
     clue_value = clue["value"]
@@ -24,38 +28,3 @@ while True:
     print(score)
     if input("Play again? y/n") == "n":
         break 
-
-
-
-# @app.route('/answerJeopardy', methods = ['GET', 'POST'])
-# def answerJeopardy():
-#     if request.method == 'GET':
-#         return ""
-#     else:   
-#         def display_clue(clue):
-#             clue_category = clue["category"]["title"]
-#             clue_value = clue["value"]
-#             clue_question = clue["question"]
-#             print(f"Category: {clue_category}")
-#             print(f"Value: {clue_value}")
-#             print(f"Question: {clue_question}")
-# def input_answer(clue):
-#     global score
-#     clue_question = clue["question"]
-#     answer_user = (input("What is "))
-#     if answer_user.lower() == clue["answer"].lower():
-#         print("Congrats!")
-#         score =+clue["value"]
-#     else:
-#         print("Sorry! That didn't work.")
-#         score =-clue["value"]
-# score=0
-# print("Welcome to the game!")
-# while True:
-#     clue_one = random.choice(data)
-#     display_clue(clue_one)
-#     input_answer(clue_one)
-#     print(score)
-#     if input("Play again? y/n") == "n":
-#         break 
-    
